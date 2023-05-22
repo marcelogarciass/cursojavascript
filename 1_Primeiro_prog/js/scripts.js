5e383a667e0347be57ffeedb8933abf1b3cadf9a
marcelogarciass <marcelogarciass@hotmail.com>
2023-05-22 16:06:50 -0300

    aula 4

---
+++ 1_Primeiro_prog/js/scripts.js
@@ -0,0 +1,14 @@
+console.log("Primeiro");
+
+console.log("Ultimo");
+
+var a = 1;
+a = 'Marcelo Teste';
+
+console.log(a);
+
+// Comentaria de linha unica
+
+/* comentario
+de 
+multiplas linhas, cuidado ao comentar pois um usuario9 malicioso podera ver em sources*/
