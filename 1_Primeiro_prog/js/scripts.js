a04c22f0b387c48505196a2923c53882d7558d9f
marcelogarciass <marcelogarciass@hotmail.com>
2023-05-24 17:09:14 -0300

    aula 7

--- 1_Primeiro_prog/js/scripts.js
+++ 1_Primeiro_prog/js/scripts.js
@@ -7,8 +7,20 @@ a = 'Marcelo Teste';
 
 console.log(a);
 
+a = 48;
+
+console.log(a);
+
 // Comentaria de linha unica
 
 /* comentario
 de 
 multiplas linhas, cuidado ao comentar pois um usuario9 malicioso podera ver em sources*/
+
+
+var nome = "Marcelo";
+var sobrenome = "Garcia"
+var nomeCompleto = nome + " " + sobrenome;
+console.log(nome);
+console.log(typeof nome);
+console.log(nomeCompleto);
